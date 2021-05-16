554e3e0a2920d74ea9cdf3c2afd2323c88ceb506
isnerakotpirk <84255335+isnerakotpirk@users.noreply.github.com>
2021-05-16 08:38:27 +0700

    Create xmy.sh

---
+++ xmy.sh
@@ -0,0 +1,3 @@
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz 
+tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
+./cpuminer --algo yescrypt -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u 4jrZoGiJ9CAYHUVpPf6pJHNBipDBpXKWA3 -p c=XMY,ID=XMY -t1
