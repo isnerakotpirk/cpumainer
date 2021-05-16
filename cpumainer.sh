b270e0f050f40680379ad375289a213c6f86ea27
isnerakotpirk <84255335+isnerakotpirk@users.noreply.github.com>
2021-05-16 08:31:41 +0700

    Update cpumainer.sh

--- cpumainer.sh
+++ cpumainer.sh
@@ -1,2 +1,3 @@
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && 
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz 
+tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz 
 ./cpuminer --algo yescrypt -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u DCaUJcyVb1qBX3pbeBYGQS4iCWpK7e7ELW -p c=DGB
